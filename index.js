800c751f3df42237107f1421555343a74e8b5e24
PJsTimeWorks <patrick.renka@icloud.com>
2025-05-05 19:53:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      background: '#f0f0f0',
+      fontFamily: 'sans-serif',
+      fontSize: '2rem'
+    }}>
+      <p>Renka is coming soon.</p>
+    </div>
+  );
+}
