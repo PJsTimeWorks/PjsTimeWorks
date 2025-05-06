64cc8141310f7f43b8a2fb835aab0dcdfb69a0f3
Patrick Renka <patrick.renka@icloud.com>
2025-05-06 08:00:21 +0000

    Clean up deprecated swcMinify from next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Optional: remove if still causing issues
 };
 
 module.exports = nextConfig;
