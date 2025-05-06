926c5f419ae44d5cd80ba966df0ff39e80718ba7
Patrick Renka <patrick.renka@icloud.com>
2025-05-06 07:38:00 +0000

    Fix: remove deprecated appDir flag for Next.js 15

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // ✅ THIS TELLS VERCEL TO USE APP ROUTER!
-  },
 };
 
 module.exports = nextConfig;
